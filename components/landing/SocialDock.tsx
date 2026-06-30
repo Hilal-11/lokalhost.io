fa387822c509a2c1e8cced54265c84e80996f3f6
Hilal-11 <hilalahmadcodedev123@gmail.com>
2026-06-30 21:38:58 +0530

    feat(landing): add social dock component

---
+++ components/landing/SocialDock.tsx
@@ -0,0 +1,32 @@
+import { Dock, DockIcon } from "@/components/ui/dock"
+export type IconProps = React.HTMLAttributes<SVGElement>
+import {
+  SiGithub,
+  SiLinkedin,
+  SiInstagram,
+} from "react-icons/si";
+import { RiTwitterXLine } from "react-icons/ri";
+import { CiGlobe } from "react-icons/ci";
+export default function SocialDock() {
+  return (
+    <div className="relative">
+      <Dock iconMagnification={60} iconDistance={100}>
+        <DockIcon className="bg-black/10 dark:bg-white/10">
+          <SiGithub size={18}/>
+        </DockIcon>
+        <DockIcon className="bg-black/10 dark:bg-white/10">
+          <SiLinkedin size={18}/>
+        </DockIcon>
+        <DockIcon className="bg-black/10 dark:bg-white/10">
+          <SiInstagram size={18}/>
+        </DockIcon>
+        <DockIcon className="bg-black/10 dark:bg-white/10">
+          <CiGlobe size={18}/>
+        </DockIcon>
+        <DockIcon className="bg-black/10 dark:bg-white/10">
+          <RiTwitterXLine size={18}/>
+        </DockIcon>
+      </Dock>
+    </div>
+  )
+}
